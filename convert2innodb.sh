637e11a3ec8e8ae23cb155c32d16815b7c696968
NPillao-palominodb <npillao@blackbirdit.com>
2014-03-13 15:50:08 -0700

    Improvements and fix syntax

--- convert2innodb.sh
+++ convert2innodb.sh
@@ -12,15 +12,15 @@
 
 ####### FUNCTIONS #########
 
-function aproove {
-  read APROOVE
-  while [[ ${APROOVE} != 'Y' ]]; do
-    if [[ ${APROOVE} == 'N' ]]; then
+function approve {
+  read APPROVE
+  while [[ ${APPROVE} != 'Y' ]]; do
+    if [[ ${APPROVE} == 'N' ]]; then
        echo "Process cancelled by user"
        exit 1
      fi	
      echo "Please type Y or N"
-     read APROOVE
+     read APPROVE
   done
 }
 
@@ -38,7 +38,7 @@ SKIPPED_TBL=()
 KEEP_OLD_DB=''
 
 
-while getopts ":bu:p:" o; do
+while getopts ":b:" o; do
     case "${o}" in
         b)
             KEEP_OLD_DB="--no-drop-old-table"
@@ -53,7 +53,7 @@ shift $((OPTIND-1))
 
 TMP_FILE=`mktemp  /tmp/.databases.XXXXX`  || { echo "There is a problem creating tmp file"; exit 1; }
 
-mysql -B -N -e "show databases" |egrep -iv 'Database|mysql|information_schema|performance_schema' > ${TMP_FILE}
+mysql -B -N -e "show databases" |egrep -iv 'mysql|information_schema|performance_schema' > ${TMP_FILE}
 
 echo ""
 echo "===== DATABASES TO CONVERT ======"
@@ -62,30 +62,32 @@ echo "================================="
 echo ""
 
 echo "You are going to alter these databases, do you agree? 'Y|N'"
-aproove
+approve
 NPK=''
 
 for DB in `cat ${TMP_FILE}`; do
 
   for TABLE in `echo "show tables" | mysql -B --skip-column-names $DB`; do
-      echo "Scanning ${DB}.${TABLE}"    
-      NPK=`echo "select constraint_name from information_schema.table_constraints where table_name = '${TABLE}' and table_schema = '${DB}' and constraint_name = 'PRIMARY';" | mysql -B`
+      echo " =============== Scanning ${DB}.${TABLE} ==============="    
+      NPK=`echo "select constraint_name from information_schema.table_constraints where table_name = '${TABLE}' and table_schema = '${DB}' and ( constraint_TYPE='PRIMARY KEY' or constraint_TYPE='UNIQUE') limit 1;" | mysql -B`
 
       if [[ -z ${NPK} ]]; then
-        echo "Skipping: This ${DB}.${TABLE} has no PK or multiple PKs"
+        echo " ================ Skipping: This ${DB}.${TABLE} has not PK ============" |tee -a db_skipped.txt
         SKIPPED_TBL+=("${DB}.${TABLE}")
 
       else
         echo "converting ${DB}.${TABLE}"
-        pt-online-schema-change  --execute ${KEEP_OLD_DB} --nocheck-replication-filters --chunk-time=0.5 --chunk-size-limit=0 --max-load=Threads_running=20 --no-check-plan --critical-load=Threads_running=100 --set-vars=innodb_lock_wait_timeout=10 --alter 'ENGINE=InnoDB' h=localhost,D=${DB},t=${TABLE}
+        pt-online-schema-change  --execute ${KEEP_OLD_DB} --nocheck-replication-filters --chunk-time=0.5 --chunk-size-limit=0 --max-load=Threads_running=20 --no-check-plan --critical-load=Threads_running=100 --alter 'ENGINE=InnoDB' h=localhost,D=${DB},t=${TABLE}
       fi
      
      # Notify we found an error and store error found
      if [ $? -ne 0 ]; then
         ERRORS+=("${DB}.${TABLE}")
-        echo "ERROR: There was a problem converting ${DB}.${TABLE} Please review"
+        echo "ERROR: There was a problem converting ${DB}.${TABLE} Please review" |tee -a db_errors.txt
         echo "       Do you want to continue with InnoDB conversion? Y|N "
-        aproove   
+        #approve
+      else
+        echo "==============  ${DB}.${TABLE} converted  ===============" |tee -a tables_converted.txt
      fi
   done
 done
@@ -96,7 +98,7 @@ if [[ ${#SKIPPED_TBL[@]} > 0  ]]; then
   echo "============================================================================"
   for i in "${SKIPPED_TBL[@]}"; do echo "$i" |tee -a no_PK_dbs.txt; done
 else
-  echo "Excelent! All tables had a PK !"
+  echo "Excellent! All tables had a PK !"
 fi
 
 
@@ -106,7 +108,7 @@ if [[ ${#ERRORS[@]} > 0  ]]; then
   echo "==========================="
   for i in "${ERRORS[@]}"; do echo "$i"|tee -a error_dbs.txt; done
 else
-  echo "Excelent! Conversion process executed without issues !"
+  echo "Excellent! Conversion process executed without issues !"
 fi
 
 # Removing temp file with databases to be converted
